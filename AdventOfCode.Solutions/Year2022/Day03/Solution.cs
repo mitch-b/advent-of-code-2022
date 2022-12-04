7aa1deeebc839d2306cdc103fd3d726894d96b0a
Mitchell Barry <mitch.barry@gmail.com>
2022-12-04 13:20:46 -0600

    Day 3 solution

--- AdventOfCode.Solutions/Year2022/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day03/Solution.cs
@@ -2,15 +2,50 @@ namespace AdventOfCode.Solutions.Year2022.Day03;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(03, 2022, "") { }
+    public Solution() : base(03, 2022, "Rucksack Reorganization")
+    {
+        _rucksacks.AddRange(Input.SplitByNewline().Select(i => new Rucksack(i)));
+    }
+
+    private List<Rucksack> _rucksacks = new List<Rucksack>();
 
     protected override string SolvePartOne()
     {
-        return "";
+        return _rucksacks
+            .Sum(r => r.SharedItems.Sum(item => GetItemTypeValue(item)))
+            .ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return _rucksacks.Chunk(3)
+            .Select<IEnumerable<Rucksack>, int>(rucksackGroup =>
+            GetItemTypeValue(rucksackGroup
+                .Select(r => r.UniqueItems)
+                .Aggregate((prev, next) => prev.Intersect(next))
+                .First()
+            )
+        ).Sum().ToString();
+    }
+
+    private int GetItemTypeValue(char item) => item switch
+    {
+        _ when (int)item >= (int)'a' => (int)item % 32,
+        _ => ((int)item % 32) + 26
+    };
+}
+
+class Rucksack
+{
+    public List<char> FirstCompartment = new List<char>();
+    public List<char> SecondCompartment = new List<char>();
+    public IEnumerable<char> SharedItems => FirstCompartment.Intersect(SecondCompartment).Distinct();
+    public IEnumerable<char> AllItems => FirstCompartment.Union(SecondCompartment);
+    public IEnumerable<char> UniqueItems => AllItems.Distinct();
+    public Rucksack(string items)
+    {
+        var compartmentSize = items.Length / 2;
+        FirstCompartment.AddRange(items.Take(compartmentSize));
+        SecondCompartment.AddRange(items.Skip(compartmentSize));
     }
 }
