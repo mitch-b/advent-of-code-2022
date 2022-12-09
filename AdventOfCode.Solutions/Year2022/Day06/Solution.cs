7438c19fc85ce555abbc0ce1fe9d94f71e370457
Mitchell Barry <mitch.barry@gmail.com>
2022-12-08 21:10:23 -0600

    Day 6 solution

--- AdventOfCode.Solutions/Year2022/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day06/Solution.cs
@@ -2,15 +2,23 @@ namespace AdventOfCode.Solutions.Year2022.Day06;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(06, 2022, "") { }
+    public Solution() : base(06, 2022, "Tuning Trouble", false) { }
 
     protected override string SolvePartOne()
     {
+        var packetLen = 4;
+        for (var i = 0; i < (Input.Length - packetLen); i++)
+            if (Input.Skip(i).Take(packetLen).Distinct().Count() == packetLen)
+                return (i + packetLen).ToString();
         return "";
     }
 
     protected override string SolvePartTwo()
     {
+        var packetLen = 14;
+        for (var i = 0; i < (Input.Length - packetLen); i ++)
+            if (Input.Skip(i).Take(packetLen).Distinct().Count() == packetLen)
+                return (i + packetLen).ToString();
         return "";
     }
 }
