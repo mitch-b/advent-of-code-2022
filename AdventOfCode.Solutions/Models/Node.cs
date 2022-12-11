4513223e659938c0050e9be2e7470f2e83aaa00c
Mitchell Barry <mitch.barry@gmail.com>
2022-12-10 21:20:42 -0600

    WIP on Day 7

---
+++ AdventOfCode.Solutions/Models/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace AdventOfCode.Solutions.Models;
+
+public class Node<T>
+{
+	public readonly List<Node<T>> Children;
+
+	public T Value { get; set; }
+	public string Label { get; set; }
+
+	public Node(T value = default, string label = "")
+	{
+		Value = value;
+		Label = label;
+        Children = new List<Node<T>>();
+    }
+
+	/// <summary>
+	/// For example: label 'A/B/C' when searched from Node A should traverse child Node B before returning Node C
+	/// </summary>
+	/// <param name="label"></param>
+	/// <returns></returns>
+	public Node<T>? TryGetNode(string label)
+	{
+		var splitBy = new[] { '/' };
+		var labelParts = label.Split(splitBy, StringSplitOptions.TrimEntries)
+			.Where(l => !string.IsNullOrWhiteSpace(l));
+		var traversedLabels = new List<string>();
+		if (label == Label || labelParts.All(string.IsNullOrWhiteSpace))
+			return this;
+		foreach (var labelPart in labelParts)
+        {
+            traversedLabels.Add(labelPart);
+            foreach (var child in Children)
+            {
+                if (labelPart.Equals(child.Label, StringComparison.CurrentCultureIgnoreCase))
+				{
+					if (labelPart == labelParts.Last())
+						return child;
+					var traversedLabel = string.Join("/", traversedLabels);
+					var subLabel = label.Replace($"/{traversedLabel}", "");
+                    return child.TryGetNode(subLabel);
+				}
+            }
+        }
+		return default;
+	}
+}
--- AdventOfCode.Solutions/Year2022/Day07/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day07/Solution.cs
@@ -1,12 +1,77 @@
+using AdventOfCode.Solutions.Models;
+
 namespace AdventOfCode.Solutions.Year2022.Day07;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(07, 2022, "") { }
+    public Solution() : base(07, 2022, "No Space Left On Device", false)
+    {
+
+    }
+
+    private void Setup()
+    {
+        _tree = new Node<long>(0, "");
+    }
+
+    private Node<long> _tree = new Node<long>(0, "");
 
     protected override string SolvePartOne()
     {
+        Setup();
+
+        var lines = Input.SplitByNewline().Skip(1).ToArray(); // skip '$ cd /'
+        var currentNodePath = "";
+        var activeNode = _tree.TryGetNode("");
+        for (var i = 0; i < lines.Count(); i++)
+        {
+            var line = lines[i];
+            var parts = line.Split(' ');
+            if (parts[0] == "$")
+            {
+                // command
+                if (parts[1] == "ls") // we are capturing info!
+                    continue;
+                if (parts[1] == "cd")
+                {
+                    if (parts[2] == "..")
+                    {
+                        // pop up tree
+                        currentNodePath = string.Join("/", currentNodePath.Split('/')[..^1]);
+                        activeNode = _tree.TryGetNode(currentNodePath);
+                        continue;
+                    }
+                    currentNodePath += $"/{parts[2]}";
+                    activeNode = activeNode!.TryGetNode(parts[2]);
+                    continue;
+                }
+            }
+            if (parts[0] == "dir")
+            {
+                // add a node (that we likely haven't traversed yet)
+                activeNode!.Children.Add(new Node<long>(default, parts[1]));
+                continue;
+            }
+
+            var fileSize = long.Parse(parts[0]);
+            var fileName = parts[1];
+            activeNode.Children.Add(new Node<long>(fileSize, fileName));
+        }
+
+        // now I have my Node<long> built out!
+
         return "";
+    }
+
+    // to-do .. thinking what i'm doing with this.
+    private long GetNodeTotalSize(Node<long> node, long totalSize = 0)
+    {
+        foreach (var child in node.Children)
+        {
+            totalSize += child.Value;
+            if (child.Children.Any()) totalSize += GetNodeTotalSize(child);
+        }
+        return totalSize;
     }
 
     protected override string SolvePartTwo()
