44ec854ae507f4673abe5412a76da890fad2a005
Mitchell Barry <mitch.barry@gmail.com>
2022-12-11 10:45:50 -0600

    WIP Day 8

--- AdventOfCode.Solutions/Year2022/Day08/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day08/Solution.cs
@@ -2,7 +2,22 @@ namespace AdventOfCode.Solutions.Year2022.Day08;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(08, 2022, "") { }
+    public Solution() : base(08, 2022, "Treetop Tree House", true)
+    {
+        SetupForest();
+    }
+
+    private void SetupForest()
+    {
+        var inputSize = Input.SplitByNewline()[0].Length;
+        _forest = new int[inputSize, inputSize];
+        var forest = Input.SplitByNewline();
+        for (var i = 0; i < forest.Length; i++)
+            for (var j = 0; j < forest.Length; j++)
+                _forest[i, j] = int.Parse(forest[i][j].ToString());
+    }
+
+    private int[,] _forest;
 
     protected override string SolvePartOne()
     {
